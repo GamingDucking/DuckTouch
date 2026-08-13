760234c7a77a1ea4fcb5e5748d2c35a942b19166
zocomputer <help@zocomputer.com>
2026-08-13 22:13:43 +0000

    Fix shared log file writes

--- src/log.rs
+++ src/log.rs
@@ -6,16 +6,17 @@
 //! Logging and terminal output macros.
 
 use std::fs::File;
-use std::sync::LazyLock;
+use std::io::Write;
+use std::sync::{LazyLock, Mutex};
 
 /// Get a handle to the log file. This is only for use by logging macros!
 ///
 /// All the logging macros print to stderr or (on Android) logcat, but this
 /// is not convenient for users who aren't accustomed to command-line tools or
 /// who don't have access to ADB, so we also write to a log file.
-pub fn get_log_file() -> &'static File {
-    static LOG_FILE: LazyLock<File> = LazyLock::new(|| {
-        File::create(crate::paths::user_data_base_path().join("touchHLE_log.txt")).unwrap()
+pub fn get_log_file() -> &'static Mutex<File> {
+    static LOG_FILE: LazyLock<Mutex<File>> = LazyLock::new(|| {
+        Mutex::new(File::create(crate::paths::user_data_base_path().join("touchHLE_log.txt")).unwrap())
     });
 
     &LOG_FILE
@@ -78,10 +79,11 @@ macro_rules! echo {
             #[cfg(not(target_os = "android"))]
             eprintln!("{}", formatted_str);
 
-            use std::io::Write;
-            let mut log_file = $crate::log::get_log_file();
-            let _ = log_file.write_all(formatted_str.as_bytes());
-            let _ = log_file.write_all(b"\n");
+            if let Ok(mut log_file) = $crate::log::get_log_file().lock() {
+                let _ = log_file.write_all(formatted_str.as_bytes());
+                let _ = log_file.write_all(b"\n");
+                let _ = log_file.flush();
+            }
         }
     };
     () => {
@@ -93,8 +95,10 @@ macro_rules! echo {
             #[cfg(not(target_os = "android"))]
             eprintln!("");
 
-            use std::io::Write;
-            let _ = $crate::log::get_log_file().write_all(b"\n");
+            if let Ok(mut log_file) = $crate::log::get_log_file().lock() {
+                let _ = log_file.write_all(b"\n");
+                let _ = log_file.flush();
+            }
         }
     }
 }
